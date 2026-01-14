b5527e04f0789f051ea6fde9a5d8585866434c96
td28101-arch <td28101@gmail.com>
2026-01-14 19:25:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import fetch from 'node-fetch';import fetch from 'node-fetch';
 import fetch from 'node-fetch';import fetch from 'node-fetch';
 import readline from 'readline-sync';
 import fetch from 'node-fetch';import fetch from 'node-fetch';
+
 import readline from 'readline-sync';
 import fetch from 'node-fetch';import fetch from 'node-fetch';
 import fetch from 'node-fetch';import fetch from 'node-fetch';
@@ -159,6 +160,7 @@ const deleteMessage = async (channelId, messageId, token) => {
         }
     }
 })();
+
 
 
 
